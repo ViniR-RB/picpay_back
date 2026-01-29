21a58deb6735ba5efeee4560c48b37a732463255
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2026-01-29 13:05:14 -0300

    feat: rename amount value object

--- src/core/value_objects/amout_value_object.ts
+++ src/core/value_objects/amout_value_object.ts
@@ -1,6 +1,6 @@
 import AmountValueObjectException from '@/core/exceptions/amount_value_object.exception';
 
-export default class AmoutValueObject {
+export default class AmountValueObject {
   private constructor(private readonly cents: number) {
     if (cents < 0) {
       throw new AmountValueObjectException('Amount cannot be negative');
@@ -8,28 +8,28 @@ export default class AmoutValueObject {
     this.cents = cents;
   }
 
-  static fromCents(cents: number): AmoutValueObject {
-    return new AmoutValueObject(cents);
+  static fromCents(cents: number): AmountValueObject {
+    return new AmountValueObject(cents);
   }
 
-  static fromReais(reais: number): AmoutValueObject {
+  static fromReais(reais: number): AmountValueObject {
     const cents = Math.round(reais * 100);
-    return new AmoutValueObject(cents);
+    return new AmountValueObject(cents);
   }
 
-  add(amount: AmoutValueObject): AmoutValueObject {
+  add(amount: AmountValueObject): AmountValueObject {
     const newCents = this.cents + amount.getCents();
-    return new AmoutValueObject(newCents);
+    return new AmountValueObject(newCents);
   }
 
-  subtract(amount: AmoutValueObject): AmoutValueObject {
+  subtract(amount: AmountValueObject): AmountValueObject {
     const newCents = this.cents - amount.getCents();
     if (newCents < 0) {
       throw new AmountValueObjectException(
         'Resulting amount cannot be negative',
       );
     }
-    return new AmoutValueObject(newCents);
+    return new AmountValueObject(newCents);
   }
 
   getCents(): number {
