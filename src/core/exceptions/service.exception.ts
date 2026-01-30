6f36368dfdfd3b9de3f6bd058d4785a3a279cd56
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2026-01-30 10:01:17 -0300

    feat: add error stack trace in service exception

--- src/core/exceptions/service.exception.ts
+++ src/core/exceptions/service.exception.ts
@@ -1,7 +1,12 @@
+import ErrorMessages from '@/core/constants/error_messages';
 import AppException from '@/core/exceptions/app_exception';
 
 export default class ServiceException extends AppException {
-  constructor(message: string, statusCode: number = 400) {
-    super(message, statusCode);
+  constructor(message: string, statusCode: number = 400, error?: Error) {
+    super(message, statusCode, error);
+  }
+
+  static unexpectedError(error?: Error) {
+    return new ServiceException(ErrorMessages.UNEXPECTED_ERROR, 500, error);
   }
 }
