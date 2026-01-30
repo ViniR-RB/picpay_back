33b272eefd4aef929d0b0d65d5a7f7854657cf9e
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2026-01-30 09:28:44 -0300

    feat: create method do find one by query in wallet repository

--- src/modules/wallet/adapters/i_wallet.repository.ts
+++ src/modules/wallet/adapters/i_wallet.repository.ts
@@ -1,6 +1,18 @@
+import AppException from '@/core/exceptions/app_exception';
 import BaseRepository from '@/core/interface/base.repository';
+import AsyncResult from '@/core/types/async_result';
 import WalletEntity from '@/modules/wallet/domain/entities/wallet.entity';
 import WalletModel from '@/modules/wallet/infra/models/wallet.model';
 
+export interface FindOneByQueryWallet {
+  selectFields?: (keyof WalletModel)[];
+  relations?: string[];
+  walletId?: string;
+}
+
 export default interface IWalletRepository
-  extends BaseRepository<WalletEntity, WalletModel> {}
+  extends BaseRepository<WalletEntity, WalletModel> {
+  findOneByQuery(
+    query: FindOneByQueryWallet,
+  ): AsyncResult<AppException, WalletEntity>;
+}
